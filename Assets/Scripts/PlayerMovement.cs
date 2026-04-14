e7b85993426175799e158d39c4da7648ff0aa965
Florentin Habermaier <s2410238024@fhooe.at>
2026-04-14 13:43:03 +0200

    PlayerMovement Borders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
     private float _speed = 5f;
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
+    private float camHeight, camWidth;
+    private Vector2 _playerExtends;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    void Start()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+        _playerExtends = GetComponent<SpriteRenderer>().bounds.extents;
+    }
+
     private void FixedUpdate()
     {
-        _rigidbody.linearVelocity = _movementInput * _speed;
+        Vector2 newPos = _rigidbody.position + _movementInput * _speed * Time.fixedDeltaTime;
+
+        newPos.x = Mathf.Clamp(newPos.x, -camWidth + _playerExtends.x, camWidth - _playerExtends.x);
+        newPos.y = Mathf.Clamp(newPos.y, -camHeight + _playerExtends.y, camHeight - _playerExtends.y);
+
+        _rigidbody.MovePosition(newPos);
     }
 
     private void OnMove(InputValue inputValue)
