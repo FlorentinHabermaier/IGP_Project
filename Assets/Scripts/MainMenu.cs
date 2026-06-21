77f26a919b8d6246f265bf614d5a301a6cfc5c49
Florentin Habermaier <s2410238024@fhooe.at>
2026-06-22 00:54:19 +0200

    ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private static readonly Vector2 MenuButtonSize = new Vector2(240f, 76f);
+
+    [Header("UI Art")]
+    [SerializeField] private Sprite primaryButtonSprite;
+    [SerializeField] private Sprite primaryButtonPressedSprite;
+    [SerializeField] private Sprite secondaryButtonSprite;
+    [SerializeField] private Sprite secondaryButtonPressedSprite;
+    [SerializeField] private Sprite buttonHoverSprite;
+    [SerializeField] private Sprite buttonDisabledSprite;
+
     void Start()
     {
-        
+        StyleButtons();
     }
 
     // Update is called once per frame
@@ -23,5 +34,79 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void StyleButtons()
+    {
+        foreach (Button button in FindObjectsByType<Button>(FindObjectsSortMode.None))
+        {
+            bool secondary = button.gameObject.name == "Quit";
+            Sprite normalSprite = secondary && secondaryButtonSprite != null ? secondaryButtonSprite : primaryButtonSprite;
+            Sprite pressedSprite = secondary && secondaryButtonPressedSprite != null ? secondaryButtonPressedSprite : primaryButtonPressedSprite;
+
+            Image image = button.GetComponent<Image>();
+            if (image != null && normalSprite != null)
+            {
+                image.sprite = normalSprite;
+                image.type = Image.Type.Simple;
+                image.preserveAspect = false;
+                image.raycastTarget = true;
+                image.color = Color.white;
+            }
+
+            RectTransform rectTransform = button.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                rectTransform.sizeDelta = MenuButtonSize;
+                if (button.gameObject.name == "Start")
+                {
+                    rectTransform.anchoredPosition = new Vector2(0f, 12f);
+                }
+                else if (button.gameObject.name == "Quit")
+                {
+                    rectTransform.anchoredPosition = new Vector2(0f, -82f);
+                }
+            }
+
+            SpriteState spriteState = button.spriteState;
+            spriteState.highlightedSprite = buttonHoverSprite;
+            spriteState.selectedSprite = buttonHoverSprite;
+            spriteState.pressedSprite = pressedSprite;
+            spriteState.disabledSprite = buttonDisabledSprite;
+            button.spriteState = spriteState;
+
+            ColorBlock colors = button.colors;
+            colors.normalColor = Color.white;
+            colors.highlightedColor = Color.white;
+            colors.pressedColor = Color.white;
+            colors.selectedColor = Color.white;
+            colors.disabledColor = Color.white;
+            colors.colorMultiplier = 1f;
+            colors.fadeDuration = 0.08f;
+            button.colors = colors;
+
+            StyleButtonLabel(button);
+        }
+    }
+
+    private void StyleButtonLabel(Button button)
+    {
+        TextMeshProUGUI tmpLabel = button.GetComponentInChildren<TextMeshProUGUI>(true);
+        if (tmpLabel != null)
+        {
+            tmpLabel.fontSize = 30;
+            tmpLabel.fontStyle = FontStyles.Bold;
+            tmpLabel.color = new Color(1f, 0.95f, 0.80f, 1f);
+            tmpLabel.raycastTarget = false;
+        }
+
+        Text label = button.GetComponentInChildren<Text>(true);
+        if (label != null)
+        {
+            label.fontSize = 30;
+            label.fontStyle = FontStyle.Bold;
+            label.color = new Color(1f, 0.95f, 0.80f, 1f);
+            label.raycastTarget = false;
+        }
     }
 }
--- Assets/Scripts/Mastermind.cs
+++ Assets/Scripts/Mastermind.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Mastermind : MonoBehaviour
 {
     public static Mastermind instance;
+    private const string MainMenuSceneName = "MainMenu";
+    private const string RuntimeMenuCardName = "RuntimeMenuCard";
+    private const string RuntimeMainMenuButtonName = "MainMenuButton";
+    private static readonly Vector2 MenuCardSize = new Vector2(430f, 360f);
+    private static readonly Vector2 MenuButtonSize = new Vector2(220f, 70f);
+    private static readonly Vector2 HudButtonSize = new Vector2(150f, 48f);
     private static readonly float[] BossSpawnTimes = { 60f, 120f, 180f, 300f };
     private static readonly int[] BossEnemyIndices = { 1, 2, 3, 4 };
     private const int FinalBossEnemyIndex = 4;
@@ -48,6 +55,15 @@ public class Mastermind : MonoBehaviour
     [SerializeField] private float bossSpawnShakeStrength = 0.3f;
     [SerializeField] private float damageFlashDuration = 0.72f;
     [SerializeField] private float damageFlashAlpha = 0.14f;
+    [Header("UI Art")]
+    [SerializeField] private Sprite menuPanelSprite;
+    [SerializeField] private Sprite titleBannerSprite;
+    [SerializeField] private Sprite primaryButtonSprite;
+    [SerializeField] private Sprite primaryButtonPressedSprite;
+    [SerializeField] private Sprite secondaryButtonSprite;
+    [SerializeField] private Sprite secondaryButtonPressedSprite;
+    [SerializeField] private Sprite buttonHoverSprite;
+    [SerializeField] private Sprite buttonDisabledSprite;
     private float TowerUpgradeCost = DefaultTowerUpgradeCost;
     private float HitSpeedUpgradeCost = DefaultHitSpeedUpgradeCost;
     private float HitDmgUpgradeCost = DefaultHitDamageUpgradeCost;
@@ -73,6 +89,8 @@ public class Mastermind : MonoBehaviour
     {
         spawner = FindFirstObjectByType<EnemySpawner>();
         InitializeScreenEffects();
+        ConfigureGameplayButtons();
+        ConfigureMenuScreens();
         CreateActiveRankenArray();
         ResetPersistentProgress();
         bosstext.text = "";
@@ -109,9 +127,13 @@ public class Mastermind : MonoBehaviour
         {
             stageCount ++;
             UpdateLedProgress();
+            int bossEnemyIndex = BossEnemyIndices[nextBossSpawnIndex];
+            bool isFinalBossSpawn = bossEnemyIndex >= FinalBossEnemyIndex;
             bosstext.enabled = true;
-            bosstext.text = "Boss " + stageCount + " spawnt";
-            spawner.SpawnBoss(stageCount, BossEnemyIndices[nextBossSpawnIndex]);
+            bosstext.text = isFinalBossSpawn
+                ? "Boss " + stageCount + " spawnt\nkill the final boss"
+                : "Boss " + stageCount + " spawnt";
+            spawner.SpawnBoss(stageCount, bossEnemyIndex);
             TriggerBossSpawnEffect();
             nextBossSpawnIndex++;
             StartCoroutine(Hide());
@@ -253,6 +275,7 @@ public int getStageCount()
         deathCount++;
         Debug.Log("Game Over");
         bosstext.enabled = false;
+        ResetScreenEffects();
         UpdateVictoryDeathCountText();
         if (victoryScreen != null)
         {
@@ -274,6 +297,7 @@ public int getStageCount()
         isPaused = false;
         isShopOpen = false;
         Debug.Log("Victory");
+        ResetScreenEffects();
         UpdateVictoryDeathCountText();
         if (deathScreen != null)
         {
@@ -308,6 +332,12 @@ public int getStageCount()
     public void RestartFromVictory()
     {
         ResetRun(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(MainMenuSceneName);
     }
 
     public void TogglePauseMenu()
@@ -591,6 +621,459 @@ public int getStageCount()
         {
             victoryScreen.SetActive(false);
         }
+    }
+
+    private void ConfigureMenuScreens()
+    {
+        ConfigureMenuScreen(
+            pauseMenu,
+            "PAUSE",
+            "Weiter",
+            true,
+            new Color(0.025f, 0.035f, 0.045f, 0.84f),
+            new Color(0.13f, 0.10f, 0.075f, 0.96f),
+            new Color(1f, 0.86f, 0.46f, 1f),
+            new Color(0.26f, 0.36f, 0.48f, 1f));
+
+        ConfigureMenuScreen(
+            deathScreen,
+            "GAME OVER",
+            "Nochmal",
+            true,
+            new Color(0.08f, 0.025f, 0.025f, 0.86f),
+            new Color(0.15f, 0.085f, 0.07f, 0.96f),
+            new Color(1f, 0.42f, 0.32f, 1f),
+            new Color(0.54f, 0.20f, 0.18f, 1f));
+
+        ConfigureMenuScreen(
+            victoryScreen,
+            "VICTORY",
+            "Neu starten",
+            true,
+            new Color(0.08f, 0.06f, 0.02f, 0.82f),
+            new Color(0.15f, 0.105f, 0.06f, 0.96f),
+            new Color(1f, 0.88f, 0.36f, 1f),
+            new Color(0.35f, 0.45f, 0.30f, 1f));
+    }
+
+    private void ConfigureMenuScreen(
+        GameObject screen,
+        string titleText,
+        string primaryButtonText,
+        bool addMainMenuButton,
+        Color overlayColor,
+        Color panelColor,
+        Color accentColor,
+        Color buttonColor)
+    {
+        if (screen == null)
+        {
+            return;
+        }
+
+        Image overlayImage = screen.GetComponent<Image>();
+        if (overlayImage != null)
+        {
+            overlayImage.color = overlayColor;
+            overlayImage.raycastTarget = true;
+        }
+
+        EnsureMenuCard(screen, panelColor, accentColor);
+        EnsureTitleBanner(screen);
+
+        if (addMainMenuButton)
+        {
+            EnsureMainMenuButton(screen);
+        }
+
+        StyleMenuTexts(screen, titleText, accentColor);
+        StyleMenuButtons(screen, primaryButtonText, buttonColor, accentColor);
+        LayoutMenuButtons(screen);
+    }
+
+    private void ConfigureGameplayButtons()
+    {
+        foreach (Button button in FindObjectsByType<Button>(FindObjectsSortMode.None))
+        {
+            if (IsInsideScreen(button.transform))
+            {
+                continue;
+            }
+
+            StyleButton(button, false, new Color(1f, 0.95f, 0.80f, 1f));
+
+            RectTransform rectTransform = button.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                rectTransform.sizeDelta = HudButtonSize;
+            }
+        }
+    }
+
+    private void EnsureMenuCard(GameObject screen, Color panelColor, Color accentColor)
+    {
+        Transform cardTransform = screen.transform.Find(RuntimeMenuCardName);
+        GameObject cardObject = cardTransform != null ? cardTransform.gameObject : null;
+
+        if (cardObject == null)
+        {
+            cardObject = new GameObject(RuntimeMenuCardName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
+            cardObject.layer = screen.layer;
+            cardObject.transform.SetParent(screen.transform, false);
+        }
+
+        cardObject.transform.SetAsFirstSibling();
+
+        RectTransform rectTransform = cardObject.GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        rectTransform.anchoredPosition = Vector2.zero;
+        rectTransform.sizeDelta = MenuCardSize;
+
+        Image image = cardObject.GetComponent<Image>();
+        if (menuPanelSprite != null)
+        {
+            image.sprite = menuPanelSprite;
+            image.type = Image.Type.Sliced;
+            image.color = Color.white;
+        }
+        else
+        {
+            image.color = panelColor;
+        }
+
+        image.raycastTarget = false;
+
+        Shadow shadow = GetOrAddShadow(cardObject);
+        shadow.effectColor = new Color(0f, 0f, 0f, 0.55f);
+        shadow.effectDistance = new Vector2(8f, -8f);
+        shadow.useGraphicAlpha = false;
+
+        Outline outline = GetOrAddComponent<Outline>(cardObject);
+        outline.effectColor = accentColor;
+        outline.effectDistance = new Vector2(3f, -3f);
+        outline.useGraphicAlpha = false;
+    }
+
+    private void EnsureTitleBanner(GameObject screen)
+    {
+        if (titleBannerSprite == null)
+        {
+            return;
+        }
+
+        const string titleBannerName = "RuntimeTitleBanner";
+        Transform bannerTransform = screen.transform.Find(titleBannerName);
+        GameObject bannerObject = bannerTransform != null ? bannerTransform.gameObject : null;
+
+        if (bannerObject == null)
+        {
+            bannerObject = new GameObject(titleBannerName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
+            bannerObject.layer = screen.layer;
+            bannerObject.transform.SetParent(screen.transform, false);
+        }
+
+        bannerObject.transform.SetSiblingIndex(1);
+
+        RectTransform rectTransform = bannerObject.GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        rectTransform.anchoredPosition = new Vector2(0f, 108f);
+        rectTransform.sizeDelta = new Vector2(390f, 86f);
+
+        Image image = bannerObject.GetComponent<Image>();
+        image.sprite = titleBannerSprite;
+        image.type = Image.Type.Simple;
+        image.color = Color.white;
+        image.raycastTarget = false;
+    }
+
+    private void EnsureMainMenuButton(GameObject screen)
+    {
+        Transform existingButtonTransform = screen.transform.Find(RuntimeMainMenuButtonName);
+        Button button = existingButtonTransform != null ? existingButtonTransform.GetComponent<Button>() : null;
+
+        if (button == null)
+        {
+            GameObject buttonObject = new GameObject(RuntimeMainMenuButtonName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(Button));
+            buttonObject.layer = screen.layer;
+            buttonObject.transform.SetParent(screen.transform, false);
+            button = buttonObject.GetComponent<Button>();
+
+            GameObject labelObject = new GameObject("Text", typeof(RectTransform), typeof(CanvasRenderer), typeof(TextMeshProUGUI));
+            labelObject.layer = screen.layer;
+            labelObject.transform.SetParent(buttonObject.transform, false);
+
+            RectTransform labelRect = labelObject.GetComponent<RectTransform>();
+            labelRect.anchorMin = Vector2.zero;
+            labelRect.anchorMax = Vector2.one;
+            labelRect.offsetMin = Vector2.zero;
+            labelRect.offsetMax = Vector2.zero;
+
+            TextMeshProUGUI label = labelObject.GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI template = screen.GetComponentInChildren<TextMeshProUGUI>(true);
+            if (template != null && template.font != null)
+            {
+                label.font = template.font;
+            }
+        }
+
+        button.onClick.RemoveListener(ReturnToMainMenu);
+        button.onClick.AddListener(ReturnToMainMenu);
+    }
+
+    private void StyleMenuTexts(GameObject screen, string titleText, Color accentColor)
+    {
+        bool titleStyled = false;
+        foreach (Text text in screen.GetComponentsInChildren<Text>(true))
+        {
+            if (IsInsideButton(text.transform))
+            {
+                continue;
+            }
+
+            text.text = titleText;
+            text.fontSize = 52;
+            text.fontStyle = FontStyle.Bold;
+            text.alignment = TextAnchor.MiddleCenter;
+            text.color = accentColor;
+            PositionMenuText(text.rectTransform, new Vector2(0f, 104f), new Vector2(380f, 72f));
+            titleStyled = true;
+            break;
+        }
+
+        TextMeshProUGUI subtitleText = null;
+        foreach (TextMeshProUGUI text in screen.GetComponentsInChildren<TextMeshProUGUI>(true))
+        {
+            if (IsInsideButton(text.transform))
+            {
+                continue;
+            }
+
+            if (!titleStyled)
+            {
+                text.text = titleText;
+                text.fontSize = 54;
+                text.fontStyle = FontStyles.Bold;
+                text.alignment = TextAlignmentOptions.Center;
+                text.color = accentColor;
+                PositionMenuText(text.rectTransform, new Vector2(0f, 104f), new Vector2(380f, 72f));
+                titleStyled = true;
+                continue;
+            }
+
+            subtitleText = text;
+        }
+
+        if (subtitleText != null)
+        {
+            subtitleText.fontSize = 30;
+            subtitleText.fontStyle = FontStyles.Bold;
+            subtitleText.alignment = TextAlignmentOptions.Center;
+            subtitleText.color = new Color(1f, 0.93f, 0.74f, 1f);
+            PositionMenuText(subtitleText.rectTransform, new Vector2(0f, 42f), new Vector2(360f, 44f));
+        }
+    }
+
+    private void PositionMenuText(RectTransform rectTransform, Vector2 position, Vector2 size)
+    {
+        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        rectTransform.anchoredPosition = position;
+        rectTransform.sizeDelta = size;
+    }
+
+    private void StyleMenuButtons(GameObject screen, string primaryButtonText, Color buttonColor, Color accentColor)
+    {
+        foreach (Button button in screen.GetComponentsInChildren<Button>(true))
+        {
+            bool isMainMenuButton = button.gameObject.name == RuntimeMainMenuButtonName;
+            SetButtonLabel(button, isMainMenuButton ? "Main Menu" : primaryButtonText);
+            StyleButton(button, isMainMenuButton, new Color(1f, 0.95f, 0.80f, 1f));
+        }
+    }
+
+    private void StyleButton(Button button, bool secondary, Color textColor)
+    {
+        Sprite normalSprite = secondary && secondaryButtonSprite != null ? secondaryButtonSprite : primaryButtonSprite;
+        Sprite pressedSprite = secondary && secondaryButtonPressedSprite != null ? secondaryButtonPressedSprite : primaryButtonPressedSprite;
+
+        Image image = button.GetComponent<Image>();
+        if (image != null)
+        {
+            if (normalSprite != null)
+            {
+                image.sprite = normalSprite;
+                image.type = Image.Type.Simple;
+                image.preserveAspect = false;
+                image.raycastTarget = true;
+                image.color = Color.white;
+            }
+            else
+            {
+                image.color = secondary ? new Color(0.54f, 0.20f, 0.18f, 1f) : new Color(0.26f, 0.36f, 0.48f, 1f);
+            }
+        }
+
+        SpriteState spriteState = button.spriteState;
+        spriteState.highlightedSprite = buttonHoverSprite;
+        spriteState.selectedSprite = buttonHoverSprite;
+        spriteState.pressedSprite = pressedSprite;
+        spriteState.disabledSprite = buttonDisabledSprite;
+        button.spriteState = spriteState;
+
+        ColorBlock colors = button.colors;
+        colors.normalColor = Color.white;
+        colors.highlightedColor = Color.white;
+        colors.pressedColor = Color.white;
+        colors.selectedColor = Color.white;
+        colors.disabledColor = Color.white;
+        colors.colorMultiplier = 1f;
+        colors.fadeDuration = 0.08f;
+        button.colors = colors;
+
+        Outline outline = button.GetComponent<Outline>();
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
+
+        SetButtonTextColor(button, textColor);
+    }
+
+    private void SetButtonLabel(Button button, string labelText)
+    {
+        TextMeshProUGUI tmpLabel = button.GetComponentInChildren<TextMeshProUGUI>(true);
+        if (tmpLabel != null)
+        {
+            tmpLabel.text = labelText;
+            tmpLabel.fontSize = 24;
+            tmpLabel.fontStyle = FontStyles.Bold;
+            tmpLabel.alignment = TextAlignmentOptions.Center;
+            tmpLabel.color = new Color(1f, 0.95f, 0.80f, 1f);
+            tmpLabel.raycastTarget = false;
+            return;
+        }
+
+        Text label = button.GetComponentInChildren<Text>(true);
+        if (label != null)
+        {
+            label.text = labelText;
+            label.fontSize = 24;
+            label.fontStyle = FontStyle.Bold;
+            label.alignment = TextAnchor.MiddleCenter;
+            label.color = new Color(1f, 0.95f, 0.80f, 1f);
+            label.raycastTarget = false;
+        }
+    }
+
+    private void SetButtonTextColor(Button button, Color textColor)
+    {
+        TextMeshProUGUI tmpLabel = button.GetComponentInChildren<TextMeshProUGUI>(true);
+        if (tmpLabel != null)
+        {
+            tmpLabel.color = textColor;
+            tmpLabel.fontStyle = FontStyles.Bold;
+            tmpLabel.raycastTarget = false;
+        }
+
+        Text label = button.GetComponentInChildren<Text>(true);
+        if (label != null)
+        {
+            label.color = textColor;
+            label.fontStyle = FontStyle.Bold;
+            label.raycastTarget = false;
+        }
+    }
+
+    private void LayoutMenuButtons(GameObject screen)
+    {
+        Button[] buttons = screen.GetComponentsInChildren<Button>(true);
+        Array.Sort(buttons, (left, right) => left.transform.GetSiblingIndex().CompareTo(right.transform.GetSiblingIndex()));
+
+        float firstY = buttons.Length > 1 ? -36f : -76f;
+        const float buttonSpacing = 82f;
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            RectTransform rectTransform = buttons[i].GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            rectTransform.pivot = new Vector2(0.5f, 0.5f);
+            rectTransform.anchoredPosition = new Vector2(0f, firstY - i * buttonSpacing);
+            rectTransform.sizeDelta = MenuButtonSize;
+        }
+    }
+
+    private bool IsInsideButton(Transform transformToCheck)
+    {
+        Transform current = transformToCheck.parent;
+        while (current != null)
+        {
+            if (current.GetComponent<Button>() != null)
+            {
+                return true;
+            }
+
+            current = current.parent;
+        }
+
+        return false;
+    }
+
+    private bool IsInsideScreen(Transform transformToCheck)
+    {
+        return IsChildOf(transformToCheck, pauseMenu)
+            || IsChildOf(transformToCheck, deathScreen)
+            || IsChildOf(transformToCheck, victoryScreen);
+    }
+
+    private bool IsChildOf(Transform transformToCheck, GameObject parent)
+    {
+        if (parent == null)
+        {
+            return false;
+        }
+
+        Transform current = transformToCheck;
+        while (current != null)
+        {
+            if (current.gameObject == parent)
+            {
+                return true;
+            }
+
+            current = current.parent;
+        }
+
+        return false;
+    }
+
+    private T GetOrAddComponent<T>(GameObject target) where T : Component
+    {
+        T component = target.GetComponent<T>();
+        if (component != null)
+        {
+            return component;
+        }
+
+        return target.AddComponent<T>();
+    }
+
+    private Shadow GetOrAddShadow(GameObject target)
+    {
+        foreach (Shadow shadow in target.GetComponents<Shadow>())
+        {
+            if (shadow.GetType() == typeof(Shadow))
+            {
+                return shadow;
+            }
+        }
+
+        return target.AddComponent<Shadow>();
     }
 
     private void TriggerBossSpawnEffect()
